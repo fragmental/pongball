e99446836111d89b03f660cd428bfda386878381
Kieran Gunn <kg45@hotmail.co.uk>
2017-04-23 00:52:27 +0100

    added boost pad script and material and spinner activation script

---
+++ Assets/scripts/entity/BoostPadForce.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostPadForce : MonoBehaviour
+{
+
+    private float force = 10.0f;
+    public Vector3 forceDirection = Vector3.zero;
+
+    //private Vector3 myStartingScale = Vector3.zero;
+    //private float scaleFactor = 1.2f;
+    private float lerpAmount = 0.05f;
+
+    /*private void Start()
+    {
+        myStartingScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, myStartingScale, lerpAmount);
+    }*/
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Ball")
+        {
+            Rigidbody rigidBody = collider.gameObject.GetComponent<Rigidbody>();
+            //Vector3 forceVector = rigidBody.transform.forward.normalized;
+            Vector3 forceVector = forceDirection;
+            forceVector.y = 0;
+            rigidBody.AddForce(forceVector * force);
+
+            /*Vector3 newScale = new Vector3(
+                myStartingScale.x * scaleFactor,
+                myStartingScale.y * scaleFactor,
+                myStartingScale.z * scaleFactor);
+
+            transform.localScale = newScale;*/
+        }
+    }
+}
---
+++ Assets/scripts/entity/SpinnerActivator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerActivator : MonoBehaviour
+{
+    public bool boostEnabled;
+    public GameObject boost;
+
+    private void Start()
+    {
+        boost.GetComponent<BoostPadForce>().enabled = false;
+        boost.GetComponent<BoostPad>().lightDisabled();
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (boostEnabled == true)
+        {
+            boost.GetComponent<BoostPadForce>().enabled = false;
+            boost.GetComponent<BoostPad>().lightDisabled();
+        }
+        else if (boostEnabled == false)
+        {
+            boost.GetComponent<BoostPadForce>().enabled = true;
+            boost.GetComponent<BoostPad>().lightEnabled();
+        }
+
+
+    }
+
+}
